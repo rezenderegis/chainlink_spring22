1c7cfbd22fc358ef4f126b31558dd7e292b2e786
rezendereigis <rezenderegis@me.com>
2022-05-07 12:12:37 -0300

    Send files

---
+++ app/client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello Smart Receipt!
+      </h1>
+    </div>
+  )
+}
+
+export default App
