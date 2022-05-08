09bf911aa8da273525302dd03028912db41fcadc
rezendereigis <rezenderegis@me.com>
2022-05-08 16:18:33 -0300

    Fix

--- app/client/src/context/TransactionContext.jsx
+++ app/client/src/context/TransactionContext.jsx
@@ -22,7 +22,7 @@ const getEthereumContract = ()=>{
 
 export const TransactionProvider = ({children}) =>{
     return (
-        <TransactionContext.Provider value={{value: 'Test transfer data transaction context Block Receipt 1'}}>
+        <TransactionContext.Provider value={{value: 'Test transfer data transaction context Block Receipt'}}>
             {children}
         </TransactionContext.Provider>
     )
