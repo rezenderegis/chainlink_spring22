a8cc2f12fc83f4df1688f62daa835ee656a43c6b
rezendereigis <rezenderegis@me.com>
2022-05-07 21:54:48 -0300

    Preparing application

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
+import {Loader, Welcome, Footer, Transactions, Services,Navbar} from './components';
 const App = () => {
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello Smart Receipt!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome"> 
+      
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   )
 }
---
+++ app/client/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+    const NavBar = () => {
+        return (
+            <h1>Navbar</h1>
+        );
+    }
+    export default NavBar;
+
