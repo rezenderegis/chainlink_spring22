fc7d71589cd46a70331cbe9405db623e474c8357
rezendereigis <rezenderegis@me.com>
2022-05-08 14:13:25 -0300

    Finishing front draft.

--- app/client/src/components/Navbar.jsx
+++ app/client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose} from 'react-icons/ai';
-
+//
 import logo from '../../images/logo.png';
 
 const NavbarItem = ({title, classprops})=>{
