357a969f810b3aee869386a3eabe781f59181bfc
rezendereigis <rezenderegis@me.com>
2022-05-10 16:47:09 -0300

    Finish Smart Contract

--- smartcontract/contracts/Transactions.sol
+++ smartcontract/contracts/Transactions.sol
@@ -1,60 +1,36 @@
-//SPDX-License-Identifier: <SPDX-License>
-//Default licence
-pragma solidity ˆ0.8.13;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
-contract Transactions {
+contract Transactions{
 
-    //Store transaction numbers. This case default integer
     uint256 transactionsCount;
 
-    //Origin addres
-    event Transfer(addres from, addres receiver, uint amount, string message, uint256 timestamp, string keyword);
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
-    //The object
-    struct TransferStruct {
-
+    struct TransferStruct{
         address sender;
-        addres receiver;
+        address receiver;
         uint amount;
         string message;
         uint256 timestamp;
         string keyword;
-
     }
 
-    //Create array of TransferStructure
-    TransferStruct[] transactions; 
+    TransferStruct[] transactions;
 
-    /**
-    Will add to blockchain
-    address will receive
-    string memory message: doen't go to blockchain
-     */
-    function addToClockChain(address payable receiver, uint amount, string memory message, string memory keyword) public  {
+    function addToBlockhain(address payable receiver, uint amount, string  memory message, string memory keyword) public{
         transactionsCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
-        /*msg.sender is a global variable to identify who ask the contract.
-        Adding data on transaction pool
-        */
-        transaction.push(TransferStructure(msg.sender, receiver, amount, message, block.timestamp, keyword));
-
-        //Send transaction
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
 
-    }   
+    }
 
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
-
+    function getAllTransactions() public view returns(TransferStruct[] memory){
         return transactions;
-
     }
 
-    public function getTransactionCount() public view returns (uint256) {
-
+    function getTransactionCount() public view returns(uint256){
         return transactionsCount;
-
     }
-
-
-
 }
--- smartcontract/scripts/deploy.js
+++ smartcontract/scripts/deploy.js
@@ -1,32 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const main = async () => {
+  
+  const trnsactionsFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionsContract = await trnsactionsFactory.deploy();
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  await transactionsContract.deployed();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  console.log("Transactions deployed to:", transactionsContract.address);
+}
+const runMain = async() =>{
 
-  await greeter.deployed();
+  try {
 
-  console.log("Greeter deployed to:", greeter.address);
+    await main();
+    process.exit(0);
+    
+  } catch (error) {
+    console.log(error)
+    process.exit(1);
+    
+  }
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+runMain();
+
