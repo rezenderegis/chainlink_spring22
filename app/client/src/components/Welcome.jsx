a1c9c0f26312bab19910a796ffe8999ea44bbc29
rezendereigis <rezenderegis@me.com>
2022-05-11 23:51:00 -0300

    Finishing screen test transfer.

--- app/client/src/components/Welcome.jsx
+++ app/client/src/components/Welcome.jsx
@@ -11,13 +11,14 @@ const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[[120px] flex justity-c
 const Input = ({placeholder, name,type,value,handleChange}) => (
     <input 
         placeholder={placeholder}
-        type = {tupe}
+        type = {type}
         value = {value}
         step = "0.0001"
         onChange = {(e) => handleChange(e,name)}
         className='my-2 2-full rounded-sm p-2 outline-none bg-tranparnte text-white border-none text-sm white-glassmorphism '
     />
 );
+
 const Welcome = () => {
     
     //We receive the connectWallet from TransactionContext
@@ -72,9 +73,9 @@ const Welcome = () => {
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
                                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                                    <SiEthereum font-size={21} color='#fff'/>
+                                    <SiEthereum fontSize={21} color='#fff'/>
                                 </div>
-                                <BsInfoCircle font-size={10} color='#fff'/>
+                                <BsInfoCircle fontSize={10} color='#fff'/>
                             </div>
                             <p className='text-white font-light text-sm'>Address</p>
                             <p className='text-white font-semibold text-lg mt-1'>Ethereum</p>
@@ -82,12 +83,29 @@ const Welcome = () => {
                         </div>
                 </div>
 
-                </div>
-
                 <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}}/>
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}}/>
+                        <Input placeholder="Keyword (GIF)" name="addressTo" type="text" handleChange={()=>{}}/>
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}}/>
+                        
+                        <div className='h-[1px] w-full bg-gray-400 my-2'/>
+                        {true 
+                            ? <Loader/>
+                            : (
+                                <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className='text-white w-full mt-2 border-[1px] p-2 border-white hover:bg-pink-900 rounde-full cursor-pointer'>
+                                Send Now
+                                </button>
+                            )
+                        }
+                </div>
 
                 </div>
 
+             
                 </div>
                 
                 </div>
